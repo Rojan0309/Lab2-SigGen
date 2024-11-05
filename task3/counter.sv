44506688e3170fc619e2dd581907719b5b3c89c5
Rojan Ragunathan <rojan.ragunathan@gmail.com>
2024-11-05 02:58:41 +0000

    modified:   task3/counter.sv 	modified:   task3/doit.sh 	new file:   task3/obj_dir/Vsigdelay 	new file:   task3/obj_dir/Vsigdelay.cpp 	new file:   task3/obj_dir/Vsigdelay.h 	new file:   task3/obj_dir/Vsigdelay.mk 	new file:   task3/obj_dir/Vsigdelay__ALL.a 	new file:   task3/obj_dir/Vsigdelay__ALL.cpp 	new file:   task3/obj_dir/Vsigdelay__ALL.d 	new file:   task3/obj_dir/Vsigdelay__ALL.o 	new file:   task3/obj_dir/Vsigdelay__Syms.cpp 	new file:   task3/obj_dir/Vsigdelay__Syms.h 	new file:   task3/obj_dir/Vsigdelay__Trace__0.cpp 	new file:   task3/obj_dir/Vsigdelay__Trace__0__Slow.cpp 	new file:   task3/obj_dir/Vsigdelay___024root.h 	new file:   task3/obj_dir/Vsigdelay___024root__DepSet_h7222714e__0.cpp 	new file:   task3/obj_dir/Vsigdelay___024root__DepSet_h7222714e__0__Slow.cpp 	new file:   task3/obj_dir/Vsigdelay___024root__Slow.cpp 	new file:   task3/obj_dir/Vsigdelay__ver.d 	new file:   task3/obj_dir/Vsigdelay__verFiles.dat 	new file:   task3/obj_dir/Vsigdelay_classes.mk 	new file:   task3/obj_dir/sigdelay_tb.d 	new file:   task3/obj_dir/sigdelay_tb.o 	new file:   task3/obj_dir/verilated.d 	new file:   task3/obj_dir/verilated.o 	new file:   task3/obj_dir/verilated_vcd_c.d 	new file:   task3/obj_dir/verilated_vcd_c.o 	modified:   task3/ram2ports.sv 	new file:   task3/sigdelay.sv 	new file:   task3/sigdelay.vcd 	new file:   task3/vbuddy.cfg

--- task3/counter.sv
+++ task3/counter.sv
@@ -4,13 +4,11 @@ module counter #(
     // interface signals
     input logic              clk,      // clock
     input logic              rst,      // reset
-    input logic              en,       // counter enable
-    input logic  [WIDTH-1:0] incr,     // increment for tick
     output logic [WIDTH-1:0] count     // clock
 );
 
 always_ff @ (posedge clk)
     if (rst) count <= {WIDTH{1'b0}};
-    else if (en)   count <= count + incr;
+    else count <= count + {{WIDTH-1{1'b0}}, 1'b1};
 
 endmodule
